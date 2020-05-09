b0d6d90436ac91107e3d4aae5984ef0bd0e46a4b
Harish C <bine0066@kmr1.local>
2020-05-09 13:13:32 +0530

    tic tak toe

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ function checkForWinner() {
   var winner
   var playerOne = getPlayerOne() //playerOne is always blue!
     //console.log('checkForWinner, playerOne is: ', playerOne)
+    //mam i din't get to know what to chnge so i wrote this 
 var computer = (playerOne === "X") ? "O" : "X"
 //there are 8 winningCombos: 
   //three rows, three columns, two diagonals
